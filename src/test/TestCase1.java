9c6d94c4424f44bb7561212fca160b81f30a3bbd
Manish Raj <manishbits34@gmail.com>
2020-04-18 21:37:39 +0530

    First Commit

---
+++ src/test/TestCase1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class TestCase1 {
+	public static void main(String[] args) {
+
+		String temp = "yes";
+		if (temp == "yes") {
+			System.out.println("This is Git Project");
+		} else {
+			System.out.println("This is not a Git Project");
+		}
+	}
+
+}
